4fb78811b2b3982fb9e1011afbd3fbdb9997a0aa
kim <bank2014@naver.com>
2022-06-17 21:37:42 +0900

    [Remove] print title in console

--- PixelEngine/Game.cpp
+++ PixelEngine/Game.cpp
@@ -66,17 +66,6 @@ int Game::init(int argc, char *argv[])
     }
     #endif
 
-    EASY_BLOCK("print ascii title");
-    std::locale ulocale(locale(), new codecvt_utf8<wchar_t>);
-    std::wifstream ifs("assets/title.txt");
-    std::locale prevlocale = ifs.imbue(ulocale);
-    #ifndef _WIN32
-    std::wcout.sync_with_stdio(false);
-    std::wcout.imbue(std::locale("en_US.utf8"));
-    #endif
-    std::wcout << ifs.rdbuf() << std::endl;
-    EASY_END_BLOCK;
-
     EASY_BLOCK("start spdlog");
     spdlog::set_level(spdlog::level::trace);
     spdlog::set_pattern("%D %T (%s:%#) [%^%l%$] \t%v");
